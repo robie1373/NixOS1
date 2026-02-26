44cca8d7fbf9e84b0c5af12fca72e4311f1021bc
robie1373 <robie1373@gmail.com>
2026-02-25 20:51:22 -0500

    extracted modules-system-common

--- hosts/nixos1/configuration.nix
+++ hosts/nixos1/configuration.nix
@@ -9,39 +9,11 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../modules/system/1password.nix
+      ../../modules/system/common.nix
     ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos1"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
-  time.timeZone = "America/New_York";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
@@ -59,9 +31,6 @@
 
   services.qemuGuest.enable = true;
   services.spice-vdagentd.enable = true;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = false;
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
@@ -82,40 +51,12 @@
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.robie = {
-    isNormalUser = true;
-    description = "Robie";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
-    #  thunderbird
-    ];
-  };
-
   # Install firefox.
   programs.firefox.enable = true;
  
-  programs.git.enable = true;
-
-  # Install neovim and make it the deafult editor system-wide
-  programs.neovim.enable = true;
-  programs.neovim.defaultEditor = true;
-
-  # Install tailscale
-  services.tailscale.enable = true;
-
-  # Allow unfree packages
-  # nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
-    "1password"
-    "1password-gui"
-  ];
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   wget
   pkgs.home-manager
   pkgs.spice-vdagent
@@ -130,7 +71,6 @@
   #   enable = true;
   #   enableSSHSupport = true;
   # };
-  nix.settings.experimental-features = ["nix-command" "flakes" ];
 
   # List services that you want to enable:
 
---
+++ modules/system/common.nix
@@ -0,0 +1,66 @@
+{ config, pkgs, ... }: {
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = false;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.robie = {
+    isNormalUser = true;
+    description = "Robie";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    #  thunderbird
+    ];
+  };
+
+  programs.git.enable = true;
+
+  # Install neovim and make it the deafult editor system-wide
+  programs.neovim.enable = true;
+  programs.neovim.defaultEditor = true;
+
+  # Install tailscale
+  services.tailscale.enable = true;
+
+  # Allow unfree packages
+  # nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
+    "1password"
+    "1password-gui"
+  ];
+
+  nix.settings.experimental-features = ["nix-command" "flakes" ];
+
+
+
+
+
+
+
+
+}
